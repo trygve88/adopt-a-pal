b4eca21bd71c3836d22a446382aa9ad65b2a9965
trygve88 <trygvejohannessen88@gmail.com>
2022-10-19 22:41:43 +0200

    r

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import {BrowserRouter as Router, Route,  Routes } from "react-router-dom";
 import Navbar from './app/components/navbar/navbar';
-import Home from "./app/views/Home";
+import Test from "./app/views/Test";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
       <Router>
         <Routes>
-          <Route path="/" element={<Home/>} />
+          <Route path="/" element={<Test/>} />
         </Routes>
       </Router>
     </div>
--- src/app/views/Test.jsx
+++ src/app/views/Test.jsx
@@ -1,27 +1,13 @@
+import { getToken } from "../api/authController";
+
 const Test = () => {
-    
 
-    const click = () => {
-        console.log("click");
-        const checkBoxes = document.getElementsByClassName('skill-check')
-
-        for (let i = 0; i < checkBoxes.length; i++) {
-            if (checkBoxes[i].checked) {
-                checkBoxes[i].checked = false;
-            }
-            else {
-                checkBoxes[i].checked = true;
-            }
-        } 
-    };
+    getToken()
 
 	return (
         <> 
-            <li><input className="skill-check form-check-input" type="checkbox"></input> </li><br/>
-            <li><input className="skill-check form-check-input" type="checkbox"></input> </li><br/>
-            <button onClick={click} className="btn btn-primary">button</button> 
+           <p>hee2222ee</p>
         </>
-       
     )
 };
 export default Test
