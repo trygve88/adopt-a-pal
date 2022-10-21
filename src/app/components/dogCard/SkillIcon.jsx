c2571dcf382117e2191ed30cd1fc7415fe004079
trygve88 <trygvejohannessen88@gmail.com>
2022-10-22 01:00:30 +0200

    w

--- src/app/components/dogCard/SkillIcon.jsx
+++ src/app/components/dogCard/SkillIcon.jsx
@@ -1,7 +1,4 @@
 const SkillIcon = ({skill}) => {
-
-    console.log(skill);
-
     return (
         <div className="p-2 d-inline">
             <img src={`/${skill}.png`} alt={skill} style={{width:'25px'}}/>
